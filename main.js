a2ea240fc0d946545efc2581712443fd70021252
Ridwan <aladebaruwa@gmail.com>
2023-08-25 22:20:31 +0100

    add

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 let student = {
     fee: 600_000,
     getTax: () => {
-        let total = 0 
-        let result = parseInt(student.fee)*10/100 + total
-        return result
+        console.log(student.result)
         
     }
 }
-console.log(student.result)
+
